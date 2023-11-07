8b5338124927faf601913544267c71cfed180101
wushenhaoyu <1192511920@qq.com>
2023-11-07 19:38:17 +0800

    完善

--- assets/script/main/UI/hudong.ts
+++ assets/script/main/UI/hudong.ts
@@ -1,14 +1,21 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node,director,PhysicsSystem2D, NodeEventType } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('hudong')
 export class hudong extends Component {
+    @property({type:Node})
+    public player:Node = null
+    p = PhysicsSystem2D.instance
     start() {
-
+        this.node.on(NodeEventType.TOUCH_START,this.touchStart,this)
     }
-
+    touchStart()
+    {
+        this.p.raycast(this.player.getPosition(),)
+    }
     update(deltaTime: number) {
         
     }
 }
-
+
+
