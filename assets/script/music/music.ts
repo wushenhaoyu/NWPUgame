00c68023ba83d4e2896dd6993732216bcccf7248
Jaydenpang2005 <67857382+Jaydenpang2005@users.noreply.github.com>
2023-11-28 23:55:16 +0800

    settings update

--- assets/script/music/music.ts
+++ assets/script/music/music.ts
@@ -1,15 +1,32 @@
-import { _decorator, Component, Node, director } from 'cc';
+import { _decorator, Component, Node, director, AudioSource } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('music')
 export class music extends Component {
+
+
+    @property({type:Node})
+    public settings:Node = null;
+
     start() {
+
+        this.node.on("switchMusic", this.switchMusic, this)
 
     }
 
     protected onLoad(): void {
         
         director.addPersistRootNode(this.node); //背景音乐不会因切换场景而重新播放
+
+    }
+
+    switchMusic() {
+
+        const audio = this.node.getComponent(AudioSource)
+
+        audio.volume = audio.volume == 0 ? 1 : 0
+
+        console.log("yoyo")
 
     }
 
