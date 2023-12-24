f329dda8fb58701f3ae4533a3b224d172de8d8fe
wushenhaoyu <1192511920@qq.com>
2023-12-24 17:21:39 +0800

    地图更新

---
+++ assets/resources/map/map/damen/10_Vehicles_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="10_Vehicles_32x32" tilewidth="32" tileheight="32" tilecount="3456" columns="32">
+ <image source="map/damen/10_Vehicles_32x32.png" width="1024" height="3456"/>
+</tileset>
---
+++ assets/resources/map/map/damen/19_Hospital_48x48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="19_Hospital_48x48" tilewidth="32" tileheight="32" tilecount="3960" columns="24">
+ <image source="map/damen/19_Hospital_48x48.png" width="768" height="5280"/>
+</tileset>
